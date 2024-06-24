912170d21bbfeaa692460b226249f3b1886ced8a
Alvaro carpio <acarpio@arrivelogistics.com>
2024-06-24 11:03:50 -0500

    add missing controller

---
+++ BiblioTech/Api/Controllers/BooksController.cs
@@ -0,0 +1,39 @@
+﻿using BiblioTech.Domain.Dto;
+using BiblioTech.Services;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace BiblioTech.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BooksController(IBookService bookService) : ControllerBase
+{
+    // GET
+    [HttpGet]
+    [SwaggerOperation("GetBooks")]
+    public async Task<IActionResult> Get()
+    {
+        var books = await bookService.GetBooks();
+        return Ok(books);
+    }
+    // GET
+    [HttpGet("{id}")]
+    [SwaggerOperation("GetBookById")]
+    public async Task<IActionResult> Get(int id)
+    {
+        var book = await bookService.GetBookById(id);
+        return Ok(book);
+    }
+    
+    
+    // POST
+    [HttpPost]
+    [SwaggerOperation("AddBook")]
+    public async Task<IActionResult> Post([FromBody] BookDto book)
+    {
+        var result = await bookService.AddBook(book);
+        return Created("", book);
+        
+    }
+}
