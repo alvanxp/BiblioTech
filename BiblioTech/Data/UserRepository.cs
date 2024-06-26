d9c318ee39abc85005896cef0ba3ddca700dddb7
Alvaro carpio <acarpio@arrivelogistics.com>
2024-06-26 10:47:07 -0500

    specify fields returned for user

--- BiblioTech/Data/UserRepository.cs
+++ BiblioTech/Data/UserRepository.cs
@@ -12,26 +12,28 @@ public class UserRepository(IOptions<ConnectionString> connectionString) : IUser
         using var connection = new SqlConnection(connectionString.Value.DefaultConnection);
         await connection.OpenAsync();
         using var command = connection.CreateCommand();
-        
-        command.CommandText = "INSERT INTO [User] (FirstName, LastName, Username, Salt, HashedPassword) VALUES (@FirstName, @LastName, @Username, @Salt, @HashedPassword)";
+
+        command.CommandText =
+            "INSERT INTO [User] (FirstName, LastName, Username, Salt, HashedPassword) VALUES (@FirstName, @LastName, @Username, @Salt, @HashedPassword)";
         command.Parameters.AddWithValue("@FirstName", userObj.FirstName);
         command.Parameters.AddWithValue("@LastName", userObj.LastName);
         command.Parameters.AddWithValue("@Username", userObj.Username);
         command.Parameters.AddWithValue("@Salt", userObj.Salt);
         command.Parameters.AddWithValue("@HashedPassword", userObj.HashedPassword);
 
-        var id =await command.ExecuteScalarAsync();
+        var id = await command.ExecuteScalarAsync();
         userObj.Id = Convert.ToInt32(id);
         return userObj;
     }
-    
-   public async Task<User> Update(User userObj)
+
+    public async Task<User> Update(User userObj)
     {
         using var connection = new SqlConnection(connectionString.Value.DefaultConnection);
         await connection.OpenAsync();
         using var command = connection.CreateCommand();
-        
-        command.CommandText = "UPDATE [User] SET FirstName = @FirstName, LastName = @LastName, Username = @Username, Salt = @Salt, HashedPassword = @HashedPassword WHERE Id = @Id";
+
+        command.CommandText =
+            "UPDATE [User] SET FirstName = @FirstName, LastName = @LastName, Username = @Username, Salt = @Salt, HashedPassword = @HashedPassword WHERE Id = @Id";
         command.Parameters.AddWithValue("@Id", userObj.Id);
         command.Parameters.AddWithValue("@FirstName", userObj.FirstName);
         command.Parameters.AddWithValue("@LastName", userObj.LastName);
@@ -41,21 +43,22 @@ public class UserRepository(IOptions<ConnectionString> connectionString) : IUser
 
         await command.ExecuteNonQueryAsync();
         return userObj;
-    } 
+    }
 
     public async Task<User> GetUserByUsername(string modelUsername)
     {
-       //access to user table using ado.net
+        //access to user table using ado.net
         using var connection = new SqlConnection(connectionString.Value.DefaultConnection);
         await connection.OpenAsync();
         using var command = connection.CreateCommand();
-        
-        command.CommandText = "SELECT * FROM [User] WHERE Username = @Username";
+
+        command.CommandText =
+            "SELECT Id, FirstName, LastName, Username, HashedPassword, Salt FROM [User] WHERE Username = @Username";
         command.Parameters.AddWithValue("@Username", modelUsername);
-        
+
         using var reader = await command.ExecuteReaderAsync();
         if (!await reader.ReadAsync()) return null;
-        
+
         return new User
         {
             Id = reader.GetInt32(0),
